365417d8b84f0f09ecc3e49c13e64bfffc034df6
Cho-El <syzzang21c@gmail.com>
2023-10-29 20:41:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    const a = await MissionUtils.Console.readLineAsync("입력해주세요 : ");
+    MissionUtils.Console.print(a);
+  }
 }
+
+const test = new App();
+test.play();
 
 export default App;
